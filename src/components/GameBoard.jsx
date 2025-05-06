6eaf7024a0124d6d56b877130fafeaa6fef29068
ImJustAPassenger <ferrauto1020@gmail.com>
2025-05-06 14:10:38 +0200

    added Game over logic

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,22 +1,4 @@
-import { useState } from "react";
-
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({onSelectSquare,turns}) {
-  let gameBoard = initialGameBoard
-
-for (const turn of turns){
-    const {square,player} = turn
-    const {row,col} = square
-
-    gameBoard[row][col]=player
-}
-
-
+export default function GameBoard({ onSelectSquare, gameBoard }) {
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -24,7 +6,12 @@ for (const turn of turns){
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={()=>onSelectSquare(rowIndex,colIndex)}  disabled={playerSymbol!==null}>{playerSymbol}</button>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react";
-export default function Player({ name, symbol,isActive }) {
-  const [nameDisplayed, setName] = useState(name);
+export default function Player({ name, symbol, isActive, onChangeName }) {
+  const [playerName, setName] = useState(name);
   const [isEditing, setEditing] = useState(false);
-  let spanPlayerName = <span className="player-name">{nameDisplayed}</span>;
+  let spanPlayerName = <span className="player-name">{playerName}</span>;
   if (isEditing) {
     spanPlayerName = (
-      <input type="text" required defaultValue={nameDisplayed} onChange={handleChange} />
+      <input
+        type="text"
+        required
+        defaultValue={playerName}
+        onChange={handleChange}
+      />
     );
   }
 
   function handleEdit() {
     setEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
   function handleChange(event) {
     setName(() => event.target.value);
   }
 
   return (
-    <li className={isActive?'active':undefined}>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {spanPlayerName}
         <span className="player-symbol">{symbol}</span>
